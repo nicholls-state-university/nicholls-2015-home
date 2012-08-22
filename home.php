9cf54111ea4fcc149d593303aa6333076f837b83
Nicholls Website Manager <nichweb@nicholls.edu>
2012-08-22 18:55:14 +0000

    Nicholls 2012 Home theme - introduce home page icon set for testing

--- home.php
+++ home.php
@@ -29,7 +29,7 @@ function nicholls_theme_home_init_action() {
 	// Remove standard widgets for home
 	if ( is_front_page() ) {
 		remove_action( 'fnbx_container_end', 'fnbx_default_widget_sidebar' );
-		add_action( 'fnbx_wp_head_after', 'nicholls_google_analytics' );
+		// add_action( 'fnbx_wp_head_after', 'nicholls_google_analytics' );
 	}
 }
 add_action( 'fnbx_child_init', 'nicholls_theme_home_init_action' );
@@ -96,6 +96,18 @@ echo $page_message;
 <?php get_sidebar() ?>
 
 	<?php do_action( 'nicholls_template_home_start', 'nicholls_template_home' ) ?><!-- START: funroe_template_home -->
+	
+	<div id="nicholls-home-list-container" class="nicholls-home-list-container-">
+		<ul id="nicholls-home-list" class="nicholls-home-list-">
+			<li id="nicholls-home-list-apply" class="nicholls-home-list-item nicholls-home-list-apply-"><a class="nicholls-home-list-item-link nicholls-home-apply-link-" href="">Apply Now</a></li>
+			<li id="nicholls-home-list-calendar" class="nicholls-home-list-item nicholls-home-list-calendar-"><a class="nicholls-home-list-item-link nicholls-home-calendar-link-" href="">Calendar</a></li>
+			<li id="nicholls-home-list-majors" class="nicholls-home-list-item nicholls-home-list-majors-"><a class="nicholls-home-list-item-link nicholls-home-majors-link-" href="">Majors &amp; Academics</a></li>
+			<li id="nicholls-home-list-tour" class="nicholls-home-list-item nicholls-home-list-tour-"><a class="nicholls-home-list-item-link nicholls-home-tour-link-" href="">Schedule A Tour</a></li>
+			<li id="nicholls-home-list-finaid" class="nicholls-home-list-item nicholls-home-list-finaid-"><a class="nicholls-home-list-item-link nicholls-home-finaid-link-" href="">Financial Aid</a></li>
+			<li id="nicholls-home-list-explore" class="nicholls-home-list-item nicholls-home-list-explore-"><a class="nicholls-home-list-item-link nicholls-home-expore-link-" href="">Expore Nicholls</a></li>
+			<li id="nicholls-home-list-social" class="nicholls-home-list-item nicholls-home-list-social-"><a class="nicholls-home-list-item-link nicholls-home-social-link-" href="">Social Media</a></li>
+		</ul>
+	</div>
 
 		<?php fnbx_layout_element_open( 'home-column-a' ) ?>
 
